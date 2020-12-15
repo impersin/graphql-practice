5d851270d6e845da955a815c75baa51d2ce46cc2
impersin <closer124@gmail.com>
2020-12-15 15:47:42 -0800

    Initialized Book schema.

---
+++ server/schema/schema.js
@@ -0,0 +1,31 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    // book => query name ex) book {}
+    book: {
+      type: BookType, // target object
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // ex) args is 'id' above
+        // code to get data from db / other source
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
