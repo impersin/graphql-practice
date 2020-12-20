121c0dfdd08fe511292743a92527479dd8bf5233
impersin <closer124@gmail.com>
2020-12-19 21:42:35 -0800

    Connected to MongoDB using mongoose.

--- server/app.js
+++ server/app.js
@@ -2,8 +2,18 @@ const express = require('express');
 // Import GraphQL module.
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(
+  'mongodb+srv://Terry:Veeh012491ct@cluster0.sditq.mongodb.net/gql-ninja',
+  { useNewUrlParser: true }
+);
+
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 // route to use graphql and need to creat schema
 app.use(
